4810e893611ea35a32d3d97e35eaa33fd2ebdde0
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2023-09-26 14:28:41 +0200

    naar de db schrijven werkt

--- felixfile.py
+++ felixfile.py
@@ -1,3 +1,22 @@
+import mysql.connector
+
 def methodenaam(eengegeven):
     print(eengegeven+": van felix")
-    return "dit is de methode van felix"
+    mydb = mysql.connector.connect(
+    host="mijndatabaselotte.mysql.database.azure.com",  #port erbij indien mac
+    port=3306,
+    user="ikalsadmin",
+    password="abcd1234ABCD!@#$",
+    database="onzedatabase"
+    )
+
+    mycursor = mydb.cursor()
+    gaan = eengegeven
+    sql = "INSERT INTO Vakantiebestemming (naam, prijs) VALUES (%s, %s)"
+    val = (gaan, 2100)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+    return "dit is de methode van felix : " + str(mycursor.rowcount)
